f235ca2f9f36a8836bfb0941057fdc0ce238ab71
Alexander Smirnov <alexsm.w@yandex.ru>
2024-10-20 20:06:00 +0300

    Slightly improved Tree by adding removeNode method

--- include/bin_tree.hpp
+++ include/bin_tree.hpp
@@ -2,6 +2,7 @@
 #define BIN_TREE_HPP
 
 #include <vector>
+#include <stack>
 #include <climits>
 #include <cstddef>
 
@@ -38,6 +39,7 @@ public:
 
     // setting
         node_t createNode(value_t, node_t left = EMPTY_NODE, node_t right = EMPTY_NODE);
+        void removeNode(node_t);
 
         void setValue(node_t, value_t);
         void setLeft(node_t nodeToReset, node_t newLeft);
@@ -53,6 +55,7 @@ public:
 private:
     node_t _root;
     std::vector<BinNode> _nodes;
+    std::stack<node_t> _unbound;
 };
 
 // definitions
@@ -60,32 +63,73 @@ private:
 #define BTREE BinTree<T>
 
 // basic methods
-    BTREE_TMPLT BTREE::BinTree(BinTree::size_t nodeCount) : _root(EMPTY_NODE), _nodes(nodeCount) {}
+    BTREE_TMPLT BTREE::BinTree(BinTree::size_t nodeCount) : _root(EMPTY_NODE), _nodes(nodeCount) {
+        for (node_t i = 0; i < nodeCount; ++i) {
+            _nodes[i].left = EMPTY_NODE;
+            _nodes[i].right = EMPTY_NODE;
+            _unbound.push(i);
+        }
+    }
 
-    BTREE_TMPLT BTREE::BinTree(const BinTree &other) : _root(other._root), _nodes(other._nodes) {}
-    BTREE_TMPLT BTREE::BinTree(BinTree &&other) : _root(other._root), _nodes(std::move(other._nodes)) {}
+    BTREE_TMPLT BTREE::BinTree(const BinTree &other) : _root(other._root), _nodes(other._nodes), _unbound(other._unbound) {}
+    BTREE_TMPLT BTREE::BinTree(BinTree &&other) : _root(other._root), _nodes(std::move(other._nodes)), _unbound(std::move(other._unbound)) {}
 
     BTREE_TMPLT BTREE &BTREE::operator=(const BinTree &other) {
         _root = other._root;
         _nodes = other._nodes;
+        _unbound = other._unbound;
         return *this;
     }
 
     BTREE_TMPLT BTREE &BTREE::operator=(BinTree &&other) {
         _root = other._root;
         _nodes = std::move(other._nodes);
+        _unbound = std::move(other._unbound);
         return *this;
+    }
+
+    BTREE_TMPLT void BTREE::swap(BinTree &other) {
+        std::swap(_root, other._root);
+        _nodes.swap(other._nodes);
+        _unbound.swap(other._unbound);
     }
 
 // setting
     BTREE_TMPLT typename BTREE::node_t BTREE::createNode(value_t value, node_t left, node_t right) {
-        _nodes.push_back({value, left, right});
-        node_t res = _nodes.size() - 1;
+        auto res = EMPTY_NODE;
+
+        if (!_unbound.empty()) {
+            res = _unbound.top();
+            _unbound.pop();
+            _nodes[res] = {value, left, right};
+        } else {
+            _nodes.push_back({value, left, right});
+            res = _nodes.size() - 1;
+        }
 
         if (_root == EMPTY_NODE) _root = res;
         else if (_root == left || _root == right) _root = res;
 
         return res;
+    }
+
+    BTREE_TMPLT void BTREE::removeNode(node_t node) {
+        std::stack<node_t> nodesToRemove;
+        nodesToRemove.push(node);
+
+        while (!nodesToRemove.empty()) {
+            auto nd = nodesToRemove.top();
+            nodesToRemove.pop();
+
+            if (nd != EMPTY_NODE) {
+                nodesToRemove.push(getRight(nd));
+                nodesToRemove.push(getLeft(nd));
+                _unbound.push(nd);
+            }
+        }
+
+        if (node == _root)
+            _root = EMPTY_NODE;
     }
 
     BTREE_TMPLT void BTREE::setValue(node_t node, value_t value) { _nodes.at(node).value = value; }
@@ -106,5 +150,8 @@ private:
     BTREE_TMPLT typename BTREE::value_t BTREE::getValue(node_t node) const { return _nodes.at(node).value; }
     BTREE_TMPLT typename BTREE::node_t BTREE::getLeft(node_t node) const { return _nodes.at(node).left; }
     BTREE_TMPLT typename BTREE::node_t BTREE::getRight(node_t node) const { return _nodes.at(node).right; }
+
+#undef BTREE_TMPLT
+#undef BTREE
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-#include <stack>
-#include <utility>
 
 #include "bin_tree.hpp"
-
-template <typename T>
-std::ostream &operator<<(std::ostream &, const BinTree<T> &);
+#include "print_bin_tree.hpp"
 
 int main(int argc, char **argv) {
-    BinTree<int> tree;
+    BinTree<int> tree(2);
     
     tree.createNode(
         3,
@@ -22,30 +18,9 @@ int main(int argc, char **argv) {
 
     std::cout << tree << '\n';
 
+    tree.removeNode(tree.getRoot());
+
+    std::cout << tree << '\n';
+
     return 0;
-}
-
-template <typename T>
-std::ostream &operator<<(std::ostream &out, const BinTree<T> &tree) {
-    std::stack<std::pair<std::size_t, typename BinTree<T>::node_t>> nodes;
-    auto root = tree.getRoot();
-    if (root == BinTree<T>::EMPTY_NODE)
-        return out;
-
-    nodes.push({0UL, root});
-
-    while (!nodes.empty()) {
-        auto pair = nodes.top();
-        nodes.pop();
-        const auto depth = pair.first;
-        const auto node = pair.second;
-        if (node != BinTree<T>::EMPTY_NODE) {
-            for (decltype(pair.first) i = 0UL; i < depth; ++i) out << "    ";
-            out << tree.getValue(node) << '\n';
-            nodes.push({depth + 1, tree.getRight(node)});
-            nodes.push({depth + 1, tree.getLeft(node)});
-        }
-    }
-
-    return out;
 }
